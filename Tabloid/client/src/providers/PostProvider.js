42e58cfde1f86b8431e97e84433d81c189fdc37b
calvincurry22 <calvincurry22@gmail.com>
2020-07-10 16:09:38 -0500

    Created an addPost function in the post provider

--- Tabloid/client/src/providers/PostProvider.js
+++ Tabloid/client/src/providers/PostProvider.js
@@ -23,6 +23,17 @@ export const PostProvider = (props) => {
             .then((res) => res.json())
     }
 
+    const addPost = (post) => {
+        return fetch("/api/post", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(post),
+        })
+            .then(getAllPosts)
+    };
+
     return (
         <PostContext.Provider value={{
             posts, getAllPosts, setPosts, getPostsByUserProfileId, getPostById
