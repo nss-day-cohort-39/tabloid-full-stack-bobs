38e93545d8b45a869452f77ca0e07e97cc11541b
Daniel Hero <danielhero@outlook.com>
2020-07-17 13:42:58 -0500

    categories by user and just categories by all posts dropdown

--- Tabloid/client/src/components/posts/PostList.js
+++ Tabloid/client/src/components/posts/PostList.js
@@ -1,21 +1,45 @@
 import React, { useContext, useEffect, useState, useRef } from "react";
 import { PostContext } from "../../providers/PostProvider";
+import { CategoryContext } from "../../providers/CategoryProvider";
 import Post from "./Post";
-import { Button, Modal, ModalHeader, ModalBody } from "reactstrap";
+import {
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+  Input,
+} from "reactstrap";
 import PostForm from "./PostForm";
 
 const PostList = () => {
-  const { getAllPosts, posts, getPostsByUserProfileId } = useContext(
-    PostContext
-  );
+  const {
+    getAllPosts,
+    posts,
+    getPostsByUserProfileId,
+    getPostsByCategory,
+    getPostsByCategoryByUser,
+  } = useContext(PostContext);
+  const { categories, getAllCategories } = useContext(CategoryContext);
   const [myView, setMyView] = useState(false);
   const [list, setList] = useState(posts);
   const user = JSON.parse(sessionStorage.getItem("userProfile"));
   const [userPosts, setUserPosts] = useState([]);
   const [modal, setModal] = useState(false);
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  const [categoryValue, setCategoryValue] = useState(0);
+  const category = useRef();
 
   const toggleButton = () => {
     setMyView(!myView);
+    setCategoryValue(0);
+  };
+
+  const toggleDropdown = () => {
+    setDropdownOpen(!dropdownOpen);
   };
 
   const toggleModal = () => {
@@ -23,18 +47,27 @@ const PostList = () => {
   };
 
   useEffect(() => {
+    getAllCategories();
     if (myView === true) {
-      // setList(userPosts)
-      getPostsByUserProfileId(user.id);
+      if (categoryValue === 0) {
+        // setList(userPosts)
+        getPostsByUserProfileId(user.id);
+      } else {
+        getPostsByCategoryByUser(user.id, parseInt(categoryValue));
+      }
       document.getElementById("postListHeader").innerHTML = "My Posts";
       document.getElementById("postToggleButton").innerHTML = "All Posts";
     } else {
-      // setList(posts)
-      getAllPosts();
+      if (categoryValue === 0) {
+        // setList(posts)
+        getAllPosts();
+      } else {
+        getPostsByCategory(parseInt(categoryValue));
+      }
       document.getElementById("postListHeader").innerHTML = "All Posts";
       document.getElementById("postToggleButton").innerHTML = "My Posts";
     }
-  }, [myView]);
+  }, [myView, categoryValue]);
 
   return (
     <>
@@ -42,6 +75,25 @@ const PostList = () => {
         My Posts
       </Button>
       <Button onClick={toggleModal}>Add Post</Button>
+
+      <Input
+        value={categoryValue}
+        type="select"
+        onChange={(e) => {
+          setCategoryValue(category.current.value);
+        }}
+        innerRef={category}
+      >
+        <option value="0">Select a Category</option>
+        {categories.map((category) => {
+          return (
+            <option key={category.id} value={category.id}>
+              {category.name}
+            </option>
+          );
+        })}
+      </Input>
+
       <div className="container">
         <h2 id="postListHeader">All Posts</h2>
         <div className="row justify-content-center">
