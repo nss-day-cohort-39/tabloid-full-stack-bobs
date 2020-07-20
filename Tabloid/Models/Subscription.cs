357021f6cfba2959999ab9d04edb62d4b791c851
Billy Blackman <blackmanbilly@gmail.com>
2020-07-20 14:09:52 -0500

    Added subscription model and repository

---
+++ Tabloid/Models/Subscription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tabloid.Models
+{
+    public class Subscription
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int SubscriberUserProfileId { get; set; }
+
+        [Required]
+        public int ProviderUserProfileId { get; set; }
+        public UserProfile SubscriberUser { get; set; }
+
+        public UserProfile ProviderUser { get; set; }
+        public DateTime BeginDateTime { get; set; }
+        public DateTime EndDateTime { get; set; }
+    }
+}
---
+++ Tabloid/Repositories/SubscriptionRepository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Tabloid.Data;
+using Tabloid.Models;
+
+namespace Tabloid.Repositories
+{
+    public class SubscriptionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public SubscriptionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Subscription> GetAll()
+        {
+            return _context.Subscription
+                            .Include(s => s.SubscriberUser)
+                            .Include(s => s.ProviderUser)
+                            .ToList();
+        }
+
+
+
+
+        public List<Subscription> GetSubscriptionsBySubscriberId(int id)
+        {
+            return _context.Subscription
+                                .Include(s => s.SubscriberUser)
+                                .Include(s => s.ProviderUser)
+                                .Where(s => s.SubscriberUserProfileId == id)
+                                .ToList();
+        }
+
+        public Subscription GetById(int id)
+        {
+            return _context.Subscription
+                            .FirstOrDefault(s => s.Id == id);
+        }
+
+        public void AddSubscription(Subscription subscription)
+        {
+            _context.Add(subscription);
+            _context.SaveChanges();
+        }
+
+        public void DeleteSubscription(int id)
+        {
+            var subscription = GetById(id);
+            _context.Subscription.Remove(subscription);
+            _context.SaveChanges();
+        }
+    }
+}
