38e9911b8bc8004c447fa005bcc7b115de0dce8e
Alex Curnow <alexcurnow@gmail.com>
2020-07-22 09:33:32 -0500

    light styling and category fix

--- Tabloid/client/src/components/comment/Comment.js
+++ Tabloid/client/src/components/comment/Comment.js
@@ -5,43 +5,48 @@ sets the currently selected comment in its parent component, and toggles the edi
 and delete modal forms in its parent component.
 */
 import React from "react";
-import { Button } from "reactstrap";
+import { Button, Card, CardBody } from "reactstrap";
 
 export const Comment = ({ c, toggleEdit, toggleDelete, setComment }) => {
   const [date, time] = c.createDateTime.split("T");
   return (
-    <div className="comment">
-      <p>
-        <strong>Subject: </strong>
-        {c.subject}
-      </p>
-      <p>
-        <strong>Comment: </strong>
-        {c.content}
-      </p>
-      <p>
-        <strong>From User: </strong>
-        {c.userProfile.displayName}
-      </p>
-      <p>{date.toLocaleString()}</p>
-      <Button
-        color="primary"
-        onClick={() => {
-          setComment(c);
-          toggleEdit();
-        }}
-      >
-        Edit
-      </Button>
-      <Button
-        color="danger"
-        onClick={() => {
-          setComment(c);
-          toggleDelete();
-        }}
-      >
-        Delete
-      </Button>
-    </div>
+    <Card key={c.id} style={{ margin: "20px" }}>
+      <CardBody>
+        <p>
+          <strong>Subject: </strong>
+          {c.subject}
+        </p>
+        <p>
+          <strong>Comment: </strong>
+          {c.content}
+        </p>
+        <p>
+          <strong>From User: </strong>
+          {c.userProfile.displayName}
+        </p>
+        <p>
+          <strong>Date Created: </strong>
+          {date.toLocaleString()}
+        </p>
+        <Button
+          color="primary"
+          onClick={() => {
+            setComment(c);
+            toggleEdit();
+          }}
+        >
+          Edit
+        </Button>
+        <Button
+          color="danger"
+          onClick={() => {
+            setComment(c);
+            toggleDelete();
+          }}
+        >
+          Delete
+        </Button>
+      </CardBody>
+    </Card>
   );
 };
